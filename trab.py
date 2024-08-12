7a05e07fb2b89f2b1cc5c22d4311caced4341784
sean carroll <mariaabotelho@yahoo.com>
2024-08-12 17:36:57 -0400

    adicionei função para sortear números e calcular a média

--- trab.py
+++ trab.py
@@ -3,4 +3,12 @@ def cumprimento(nome):
 
 print(cumprimento("Maria Antonia Botelho"))
 
+import random
+def sorteia_media():
+    numeros = [random.randint(1, 100) for _ in range(3)]
+    media = sum(numeros) / len(numeros)
+    return media
+
+print(f"a média dos números sorteados é: {sorteia_media()}")
+
 
